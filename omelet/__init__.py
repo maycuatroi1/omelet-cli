9fc8c9827fe0a608de4fc0dccd9e3c9884abcf88
Nguyen Anh Binh <socrat.nguyenanhbinh@gmail.com>
2026-01-30 08:49:24 +0700

    refactor: remove image_uploader and update exports for Ghost integration
    - Replace ImageUploader with GCSUploader in exports
    - Add GhostClient to package exports
    - Add pyjwt and python-dotenv dependencies

--- omelet/__init__.py
+++ omelet/__init__.py
@@ -6,7 +6,8 @@ __version__ = "0.1.0"
 
 from .cli import cli
 from .markdown_processor import MarkdownProcessor
-from .image_uploader import ImageUploader
+from .gcs_uploader import GCSUploader
 from .config import Config
+from .ghost_client import GhostClient
 
-__all__ = ['cli', 'MarkdownProcessor', 'ImageUploader', 'Config']
+__all__ = ['cli', 'MarkdownProcessor', 'GCSUploader', 'Config', 'GhostClient']
